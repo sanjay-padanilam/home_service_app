15b57d7e2498d6e8a06d8c0fc5cc28f4e914bbc0
sanjay-padanilam <sanjaysaji193@gmail.com>
2024-12-08 09:53:54 +0530

    homescreen

--- lib/view/homescreen/home_screen.dart
+++ lib/view/homescreen/home_screen.dart
@@ -1,53 +1,93 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool ismore = false;
+
   Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
     return Scaffold(
-      drawer: Drawer(),
-      appBar: AppBar(
-        title: Text("homescren"),
-      ),
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(hintText: 'Search..'),
-          ),
-          Flexible(
-            child: GridView.builder(
-              itemCount: 8,
-              gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-              itemBuilder: (context, index) => Padding(
+        body: CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+            child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(
+              10,
+              (index) => Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
-                  height: 100,
-                  width: 100,
+                  height: size.height / 4,
+                  width: size.width / 1,
                   color: Colors.red,
                 ),
               ),
             ),
           ),
-          Flexible(
-            child: ListView.builder(
-              itemCount: 14,
-              itemBuilder: (context, index) => ListTile(
-                leading: CircleAvatar(),
-                title: Text('text'),
-                subtitle: Text('text'),
+        )),
+        SliverToBoxAdapter(
+          child: Text("Services"),
+        ),
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              Container(
+                height: ismore ? 300 : 200,
+                width: double.infinity,
+                color: Colors.green,
+                child: GridView.builder(
+                  itemCount: ismore ? 11 : 8,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      color: Colors.yellow,
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    ismore = !ismore;
+                    setState(() {});
+                  },
+                  child: Text('seee more'))
+            ],
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Text("other Services"),
+        ),
+        SliverToBoxAdapter(
+            child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(
+              10,
+              (index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: size.height / 4,
+                  width: size.width / 1,
+                  color: Colors.red,
+                ),
               ),
             ),
-          )
-        ],
-      ),
-      bottomNavigationBar: NavigationBar(destinations: [
-        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-        // NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-        NavigationDestination(icon: Icon(Icons.person), label: 'profile'),
-      ]),
-    );
+          ),
+        )),
+      ],
+    ));
   }
 }
